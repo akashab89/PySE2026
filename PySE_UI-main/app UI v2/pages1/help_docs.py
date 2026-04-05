4896433fd50ef4392333682eb551a644ee111509
Kishor Gurung <69380182+Snotboggie109@users.noreply.github.com>
2026-04-05 22:08:22 +0200

    Delete PySE_UI-main directory

--- PySE_UI-main/app UI v2/pages1/help_docs.py
+++
@@ -1,6 +0,0 @@
-import streamlit as st
-
-def show():
-    st.header("Help & Documentation")
-    st.markdown("Learn how the app works and access the PDF guide.")
-    st.markdown("Help and Documentation content coming soon!")
--- PySE_UI-main/pages1/Utils1/info1.py
+++
@@ -1,5 +0,0 @@
-import streamlit as st
-
-def create_table():
-    st.write("This is where the table will be created based on the existing data.")
-    st.write("Table for Nodes (with attributes) and Edges tables (according to type) will be displayed here.")
